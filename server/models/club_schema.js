427373b7296d528827a6808823db6095e14e359b
vinh nguyen <vinhnguyen122005@gmail.com>
2025-07-25 21:01:34 -0700

    Updated club schema to inherit from user schema

--- server/models/club_schema.js
+++ server/models/club_schema.js
@@ -1,37 +1,11 @@
 const mongoose = require('mongoose');
+const User = require('./users_schema');
 
 const clubSchema = new mongoose.Schema({
-    clubId: {
-		type: String,
-		unique: true,
-	},
-
-	name: {
-		type: String,
-		required: true,
-		trim: true
-	}, 
-
-	email: {
-		type: String,
-		required: true,
-		unique: true,
-	}, 
-	
-	password: {
-		type: String,
-		required: true,
-	},
-
-	bio: {
-		type: String,
-		default: ""
-	},
-
-	profilePic: {
-		type: String,
-		default: ""
-    }
+    clubId: {type: String, unique: true},
+	bio: {type: String, default: ""},
+	profilePic: {type: String, default: ""}
 });
 
-module.exports = mongoose.model('Club', clubSchema);
+const Club = User.discriminator('club', clubSchema)
+module.exports = Club;
