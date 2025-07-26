9dccaed8285c8b88d8abb31541aff69271c8e94b
alex <alatienza@ucsd.edu>
2025-07-26 05:09:40 -0700

    clean up round 1

--- client/src/app/components/NavBar.jsx
+++ client/src/app/components/NavBar.jsx
@@ -1,14 +1,17 @@
-import styles from './navbar.css'
+import styles from './NavBar.module.css';
+import { LogOut, Home, Search, Bookmark, PlusSquare, User} from 'react-feather';
 
 export default function NavBar() {
-    return <nav>
-        <a href="/" className="site-logo">Site Logo</a>
-        <ul>
-            <li><a href="/home">Home</a></li>
-            <li><a href="/saved">Saved</a></li>
-            <li><a href="/create">Create</a></li>
-            <li><a href="profile">Profile</a></li>
+    return (
+    <nav className={styles.navBar}>
+        <a href="/" className={styles.siteLogo}>Site Logo</a>
+        <ul className={styles.navMiddleItems}>
+            <li className={styles.navItem}><Home className={styles.navIcon} /> <a className={styles.navLink} href="/home">Home</a></li>
+            <li className={styles.navItem}><Search className={styles.navIcon} /> <a className={styles.navLink} href="/search">Search</a></li>
+            <li className={styles.navItem}><PlusSquare className={styles.navIcon} /> <a className={styles.navLink} href="/create">Create</a></li>
+            <li className={styles.navItem}><Bookmark className={styles.navIcon} /> <a className={styles.navLink} href="/saved">Saved</a></li>
+            <li className={styles.navItem}><User className={styles.navIcon} /> <a className={styles.navLink} href="profile">Profile</a></li>
         </ul>
-        <button>Log Out</button>
+        <button className={styles.navLogOut}><LogOut className={styles.navIcon} /> <span>Log Out</span></button>
     </nav>
-}
+)};
--- client/src/app/components/events/EventCarousel.jsx
+++ client/src/app/components/events/EventCarousel.jsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
 import { ChevronLeft, ChevronRight, ArrowRight } from "react-feather";
@@ -9,6 +9,10 @@ import "swiper/css";
 import styles from './EventCarousel.module.css'
 
 const EventCarousel = ({ category, events }) => {
+    const categoryID = category.replace(/\s+/g, "-").toLowerCase(); // e.g. "free-food"
+    const prevRef = useRef(null);
+    const nextRef = useRef(null);
+
     return (
         <section className={styles.carouselSection}>
             <header className={styles.carouselHeader}>
@@ -16,14 +20,14 @@ const EventCarousel = ({ category, events }) => {
                 <p className={styles.carouselSeeAll}>See All <ArrowRight className={styles.carouselSeeAllIcon}/></p>
             </header>
             <div className={styles.carouselContent}>
-                <button className="carousel-left" id={styles.carouselLeft}><ChevronLeft className={styles.carouselLeftIcon}/></button>
+                <button className={styles.carouselLeft} id={`carousel-left-${categoryID}`}><ChevronLeft className={styles.carouselLeftIcon}/></button>
                 <Swiper
                     slidesPerView={"auto"}
                     slidesPerGroup={5}
                     spaceBetween={16}
                     navigation={{
-                        nextEl: ".carousel-right",
-                        prevEl: ".carousel-left",
+                        nextEl: `#carousel-right-${categoryID}`,
+                        prevEl: `#carousel-left-${categoryID}`,
                     }}
                     modules={[Navigation]}
                     className={styles.carouselSwiper}
@@ -34,7 +38,7 @@ const EventCarousel = ({ category, events }) => {
                         </SwiperSlide>
                     ))}
                 </Swiper>
-                <button className="carousel-right" id={styles.carouselRight}><ChevronRight className={styles.carouselRightIcon}/></button>
+                <button className={styles.carouselRight} id={`carousel-right-${categoryID}`}><ChevronRight className={styles.carouselRightIcon}/></button>
             </div>
                 
         </section>
