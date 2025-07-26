6b846dbd6ee56a3098f6892d8ab862bd40d2b2fa
Arlene Garcia <arg030@ucsd.edu>
2025-07-25 17:19:30 -0700

    first attempt using react

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,4 +1,23 @@
 import Card from "./components/events/eventCard";
+import React from 'react';
+import Header from './components/Header';
+import ProfileCard from './components/ProfileCard';
+import EventsSection from './components/EventsSection';
+
+/*
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <ProfileCard />
+        <EventsSection />
+      </main>
+    </div>
+  );
+}
+*/
+
 
 function App(){
     return(
