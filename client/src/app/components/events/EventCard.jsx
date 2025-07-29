f9c5df292df93616fa977b6bade4c223cdbc8d20
Arlene Garcia <146496596+arlenecse@users.noreply.github.com>
2025-07-29 11:55:03 -0700

    merged home page branch and made edits

--- client/src/app/components/events/EventCard.jsx
+++ client/src/app/components/events/EventCard.jsx
@@ -1,15 +1,43 @@
+"use client"
 
-function Card(){
-    return(
-        <div className="card">
-            <img className="card-image" src="https://via.placeholder.com/150" alt= "event image"></img>
-            <h2 className="card-title" > Event Title</h2>
-            <p className="org-title">Org</p>
-            {/* <p>Event Name</p>
-            <p>Location</p>
-            <p>Time</p> */}
-        </div>
-    );
-}
+import React from "react";
+import { Calendar, MapPin } from "react-feather";
+import styles from './EventCard.module.css';
 
-export default Card
+const EventCard = ({ event }) => {
+    // Event should contain the following metadata
+    const {
+        eventCover, eventTitle, eventOwner,
+        eventDate, eventTime, eventLocation,
+        eventDescription, eventTags, eventSrc
+    } = event;
+
+    function formatDate(inputDate) {
+        const date = new Date(inputDate);
+        const options = {
+            weekday: "short",
+            month: "short",
+            day: "numeric",
+            hour: "numeric",
+            minute: "2-digit",
+            hour12: true
+        };
+        return date.toLocaleString("en-US", options).replace(",", "");
+    }
+
+    return (
+        <article className={styles.eventCard}>
+            <div className={styles.eventCover}>
+                <img className={styles.eventCoverImage} src={eventCover} alt={`${eventTitle} Cover Image`}/>
+            </div>
+            <section className={styles.eventInfo}>
+                <h3 className={styles.eventTitle}>{eventTitle}</h3>
+                <p className={styles.eventOwner}>{eventOwner}</p>
+                <span className={styles.eventLocation}><MapPin className={styles.eventLocationIcon}/> {eventLocation}</span>
+                <time className={styles.eventDate}><Calendar className={styles.eventDateIcon}/> {formatDate(eventDate)}</time>
+            </section>
+        </article>
+    )
+};
+
+export default EventCard;
