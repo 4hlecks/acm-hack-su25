f8a70dc8d4929ba5362f3b9d83cdeb090b1fa579
alex <alatienza@ucsd.edu>
2025-09-08 00:26:29 -0700

    added search bar

--- client/src/app/components/NavBar.jsx
+++ client/src/app/components/NavBar.jsx
@@ -1,17 +1,21 @@
 import styles from './NavBar.module.css';
 import { LogOut, Home, Search, Bookmark, PlusSquare, User} from 'react-feather';
+import SearchBar from './SearchBar'
 
 export default function NavBar() {
     return (
     <nav className={styles.navBar}>
-        <a href="/" className={styles.siteLogo}>Site Logo</a>
-        <ul className={styles.navMiddleItems}>
-            <li className={styles.navItem}><Home className={styles.navIcon} /> <a className={styles.navLink} href="/home">Home</a></li>
-            <li className={styles.navItem}><Search className={styles.navIcon} /> <a className={styles.navLink} href="/search">Search</a></li>
-            <li className={styles.navItem}><PlusSquare className={styles.navIcon} /> <a className={styles.navLink} href="/create">Create</a></li>
-            <li className={styles.navItem}><Bookmark className={styles.navIcon} /> <a className={styles.navLink} href="/saved">Saved</a></li>
-            <li className={styles.navItem}><User className={styles.navIcon} /> <a className={styles.navLink} href="profile">Profile</a></li>
-        </ul>
-        <button className={styles.navLogOut}><LogOut className={styles.navIcon} /> <span>Log Out</span></button>
+        <div className={styles.leftSide}>
+            <a href="/" className={styles.siteLogo}>current</a>
+            <SearchBar />
+        </div>
+        <div className={styles.rightSide}>
+            <ul className={styles.navMiddleItems}>
+                <li className={styles.navItem}><a className={styles.navLink} href="/saved"><Bookmark className={styles.navIcon} /> Saved</a></li>
+                <li className={styles.navItem}><a className={styles.navLink} href="/create"><PlusSquare className={styles.navIcon} /> Create</a></li>
+                <li className={styles.navItem}><a className={styles.navLink} href="profile"><User className={styles.navIcon} /> Profile</a></li>
+            </ul>
+            <button className={styles.navLogOut}><LogOut className={styles.navIcon} /> <span>Log Out</span></button>
+        </div>
     </nav>
 )};
