7f661facbf0adacca2eea286ad7fa9d5982f7555
alex <alatienza@ucsd.edu>
2025-09-09 23:37:06 -0700

    fix merge conflicts

--- client/src/app/components/navbar/NavBar.jsx
+++ client/src/app/components/navbar/NavBar.jsx
@@ -1,21 +1,65 @@
+'use client';
+
+import { useRouter, usePathname } from 'next/navigation';
 import styles from './NavBar.module.css';
 import { LogOut, Home, Search, Bookmark, PlusSquare, User} from 'react-feather';
 import SearchBar from '../SearchBar'
+import Link from 'next/link';
 
 export default function NavBar() {
+    const router = useRouter();
+    const pathname = usePathname();
+
+    const handleLogout = async () => {
+        try {
+        localStorage.removeItem('token');
+        localStorage.removeItem('user');
+        router.push('/login');
+        } catch (err) {
+        console.error('Logout failed:', err);
+        }
+    };
+
     return (
     <nav className={styles.navBar}>
         <div className={styles.leftSide}>
-            <a href="/" className={styles.siteLogo}>current</a>
+            <Link href="/" className={styles.siteLogo}>current</Link>
             <SearchBar />
         </div>
         <div className={styles.rightSide}>
             <ul className={styles.navMiddleItems}>
-                <li className={styles.navItem}><a className={styles.navLink} href="/saved"><Bookmark className={styles.navIcon} /> Saved</a></li>
-                <li className={styles.navItem}><a className={styles.navLink} href="/create"><PlusSquare className={styles.navIcon} /> Create</a></li>
-                <li className={styles.navItem}><a className={styles.navLink} href="profile"><User className={styles.navIcon} /> Profile</a></li>
+                <li>
+                <Link
+                    href="/saved"
+                    className={`${styles.navItem}
+                                ${pathname === '/saved'
+                                ? styles.activeLink : ''}`}
+                >
+                    <Bookmark className={styles.navIcon} /> Saved
+                </Link>
+                </li>
+                <li>
+                <Link
+                    href="/add-event"
+                    className={`${styles.navItem}
+                                ${pathname === '/add-event'
+                                ? styles.activeLink : ''}`}
+                >
+                    <PlusSquare className={styles.navIcon} /> Create
+                </Link>
+                </li>
+                <li>
+                <Link
+                    href="/profile_page"
+                    className={`${styles.navItem}
+                                ${pathname === '/profile_page'
+                                ? styles.activeLink : ''}`}
+                >
+                    <User className={styles.navIcon} /> Profile
+                </Link>
+                </li>
             </ul>
-            <button className={styles.navLogOut}><LogOut className={styles.navIcon} /> <span>Log Out</span></button>
+            <button onClick={handleLogout} className={styles.navLogOut}><LogOut className={styles.navIcon} /> <span>Log Out</span></button>
         </div>
     </nav>
 )};
--- client/src/app/components/navbar/TabBar.jsx
+++ client/src/app/components/navbar/TabBar.jsx
@@ -1,14 +1,56 @@
+'use client';
+
 import styles from './TabBar.module.css';
+import { useRouter, usePathname } from 'next/navigation';
 import { LogOut, Home, Search, Bookmark, PlusSquare, User} from 'react-feather';
+import Link from 'next/link';
 
 export default function TabBar() {
+    const router = useRouter();
+    const pathname = usePathname();
+
     return (
     <nav className={styles.navBar}>
         <ul className={styles.navMiddleItems}>
-            <li className={styles.navItem}><a className={styles.navLink} href="/home"><Home className={styles.navIcon} />Home</a></li>
-            <li className={styles.navItem}><a className={styles.navLink} href="/create"><PlusSquare className={styles.navIcon} />Create</a></li>
-            <li className={styles.navItem}><a className={styles.navLink} href="/saved"><Bookmark className={styles.navIcon} />Saved</a></li>
-            <li className={styles.navItem}><a className={styles.navLink} href="profile"><User className={styles.navIcon} />Profile</a></li>
+            <li>
+            <Link
+                href="/"
+                className={`${styles.navItem}
+                            ? styles.activeLink : ''}`}
+            >
+                <Home className={styles.navIcon} /> Home
+            </Link>
+            </li>
+            <li>
+            <Link
+                href="/saved"
+                className={`${styles.navItem}
+                            ${pathname === '/saved'
+                            ? styles.activeLink : ''}`}
+            >
+                <Bookmark className={styles.navIcon} /> Saved
+            </Link>
+            </li>
+            <li>
+            <Link
+                href="/add-event"
+                className={`${styles.navItem}
+                            ${pathname === '/add-event'
+                            ? styles.activeLink : ''}`}
+            >
+                <PlusSquare className={styles.navIcon} /> Create
+            </Link>
+            </li>
+            <li>
+            <Link
+                href="/profile_page"
+                className={`${styles.navItem}
+                            ${pathname === '/profile_page'
+                            ? styles.activeLink : ''}`}
+            >
+                <User className={styles.navIcon} /> Profile
+            </Link>
+            </li>
         </ul>
     </nav>
 )};
