f5f8c25b6d02c51ad5ddf54fec6f0535356a3eca
alatienza@ucsd.edu <alatienza@ucsd.edu>
2025-09-14 12:21:13 -0700

    Layout css

--- client/src/app/admin/dashboard/page.jsx
+++ client/src/app/admin/dashboard/page.jsx
@@ -1,9 +1,12 @@
+import styles from './page.module.css';
+
 export default function Dashboard() {
     return (
         <>
             <h1>Dashboard</h1>
-            test
-            test
+            <h2>Overview</h2>
+            <h2>Send Announcement</h2>
+            <h2>Account Requests</h2>
         </>
     );
 }
