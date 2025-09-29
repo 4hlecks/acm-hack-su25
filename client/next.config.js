2b3dbd7dea90d8f742feb2064c336836db65a6d1
Vinh Nguyen <vinnynguyen122005@gmailcom>
2025-09-29 00:02:46 -0700

    Test

--- client/next.config.js
+++ client/next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_BASE || 'http://localhost:5001'}/api/:path*`,
+      },
+    ];
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
